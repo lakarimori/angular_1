e229b368b0684381b268610faaa825859bd66967
lakarimori <arimoriakio@gmail.com>
2021-01-20 21:30:01 -0300

    testing property biding and string interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  allowServer: boolean = false;
+  idTest = 'test';
+  styleTest = `
+  width: 10px;
+  height: 400px;
+  `;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allowServer = true;
+    }, 2000);
   }
 
+  ngOnInit(): void {}
 }
