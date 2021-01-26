4f6cec0097a039d62353daf4b35247ce8e4a4614
lakarimori <arimoriakio@gmail.com>
2021-01-25 21:54:35 -0300

    resolucao do exercicio

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  toggleButton: boolean = false;
+  counter: number = 0;
+  counters: Array<number> = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public onButtonClick() {
+    this.counter++;
+    this.toggleButton = !this.toggleButton;
+    this.counters.push(this.counter);
   }
-
 }
